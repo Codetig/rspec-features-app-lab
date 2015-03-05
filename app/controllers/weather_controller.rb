67f52c03fb9cdac23c907ee0611aa802b99efb9d
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-03-04 23:22:03 -0800

    rspec test2 added and passed

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,6 +4,11 @@ class WeatherController < ApplicationController
   end
 
   def show
+    if params[:zipcode] == "94702"
+      @message = ["Sun's gonna be shinning!", "No umbrella needed."]
+    else
+      @message = ["Oh the pouring rain!", "Grab your umbrella!"]
+    end
   end
 
   def search
--- spec/features/visitor_checks_the_weather_spec.rb
+++ spec/features/visitor_checks_the_weather_spec.rb
@@ -1,13 +1,10 @@
 require "rails_helper"
 
 feature "Visitor checks the weather" do
-    before do
-        visit root_path
-    end
     
   scenario "sunny" do
     #setup
-    
+    visit root_path
 
     #excercise
     fill_in "What's your zipcode?", with: "94702" 
@@ -17,4 +14,19 @@ feature "Visitor checks the weather" do
     expect(page).to have_content("Sun's gonna be shinning!")
     expect(page).to have_content("No umbrella needed.")
   end
+
+  scenario "rainy" do
+    #setup
+    visit root_path
+
+    #excercise
+    fill_in "What's your zipcode?", with: "11420" 
+    click_button "Is it going to rain?"
+
+    #verify
+    expect(page).to have_content("Oh the pouring rain!")
+    expect(page).to have_content("Grab your umbrella!")
+  end
+
+
 end
